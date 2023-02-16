a2f3ef791f5ff1b99a07e2bccda177efa40d1e10
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-02-16 23:27:00 +0800

    :bug: Fix cookies

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from contextlib import suppress
-from functools import reduce
 import http.client
 import json
 import os
+import sys
+from contextlib import suppress
+from functools import reduce
 from random import randint, random
 from time import sleep, time
 
@@ -12,25 +13,11 @@ from tqdm import tqdm
 
 T = 0.5
 
-cookies = {
-    "_s_tentry": "passport.weibo.com",
-    "ALF": "1699608779",
-    "Apache": "8299455135346.2705.1638959679478",
-    "cross_origin_proto": "SSL",
-    "login_sid_t": "4b917bd89ec5e1ef39fe8c41fb741fc0",
-    "PC_TOKEN": "b8816542d9",
-    "SCF": "Aqu9EpHWJO_tfcs5RlPDAY1rk5Ak1eaE4fItGIWdKQ3h7VfbvCtj4zMmVucFecWFf1DVmAwU--qiW-g3aEfPdgA.",
-    "SINAGLOBAL": "8299455135346.2705.1638959679478",
-    "SSOLoginState": "1665137199",
-    "SUB": "_2A25OaLUEDeRhGeNP4loX9y7LyD2IHXVtH6HMrDV8PUNbmtAKLU7VkW9NTlu_2yYmd4V4cqZ3VQVORGLel6l4-gc8",
-    "SUBP": "0033WrSXqPxfM725Ws9jqgMF55529P9D9WFVySww9wXXWoLfijNv8KiL5JpX5KMhUgL.Fo-p1KncS05Ne022dJLoI7LJMJHL9h57eh5t",
-    "ULV": "1638959679479:1:1:1:8299455135346.2705.1638959679478:",
-    "WBPSESS": "j6tioVbCBSNDSDeYczb8JsDMiVglMcEn53xad8obbDAR20Lha-bLe-SODNPFDWvvA2yLZujF2Obu0bddvtonvztIU51T0FiNTHA23y6vm1U0KoFP-Gl3TAcmJ77jA6iqBqwKBWQAkatNx9CiCcshXg==",
-    "XSRF-TOKEN": "6O6BVzPiSvLEIXnObxidFBLI",
-}
+with open("cookies.json") as f:
+    cookies = json.load(f)
 
 
-def get_response(url):
+def get_response(url: str) -> requests.models.Response:
     j = 0
     try:
         response = requests.get(url, cookies=cookies)
@@ -47,7 +34,7 @@ def get_response(url):
     return response
 
 
-def dump_json(uid):
+def dump_json(uid: int) -> None:
     fname = os.path.join("data", str(uid) + "-data.json")
     if not os.path.isfile(fname):
         data = []
@@ -94,8 +81,7 @@ def dump_json(uid):
 
 if __name__ == "__main__":
     t0 = time()
-    uids = [3766748450]
-    for uid in uids:
+    for uid in map(int, sys.argv[1:]):
         dump_json(uid)
     dt = time() - t0
     print(dt / 3600)
